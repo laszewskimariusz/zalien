a7f0a2dcfcfdd7a90c0a0edec14d149e4542cd1b
zatto84 <laszewskimariusz@gmail.com>
2016-04-26 16:47:39 +0100

    make it better

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from .models import Greeting
-
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
 
 
-def db(request):
 
-    greeting = Greeting()
-    greeting.save()
 
-    greetings = Greeting.objects.all()
+def home(request):
 
-    return render(request, 'db.html', {'greetings': greetings})
+    title = "Hi world"
+    context = {
+        "title": title,
 
+    }
+    return render(request, "home.html", context)
