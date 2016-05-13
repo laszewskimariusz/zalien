e125de740d96fbf9699e337f15ebb88fd536c4d2
zatto84 <laszewskimariusz@gmail.com>
2016-05-13 12:16:49 +0100

    smtp

--- zalien/settings.py
+++ zalien/settings.py
@@ -30,11 +30,13 @@ DEBUG = True
 # USTAWIENIA EMAIL
 
 
-EMAIL_HOST =''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = ''
-EMAIL_USE_TLS = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'Whatever <whatever@example.com>'
 
 
 
